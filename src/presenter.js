617339384046382e22dc935350086c24f88ea664
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-06 09:54:58 -0400

    Creamos el formulario crear nota

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
 const formBuscar = document.querySelector("#form-buscar");
 const tituloBuscar = document.getElementById("titulo-buscar");
 const divBuscar = document.querySelector("#div-buscado");
+const formulario = document.querySelector("#formulario");
+const tituloInput = document.querySelector("#titulo");
+const textoInput = document.querySelector("#texto");
+const divNotas = document.querySelector(".div-notas");
 
 formBuscar.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(tituloBuscar.value);
   divBuscar.innerHTML = "<p>" + tituloBuscar.value + "</p>";
 });
